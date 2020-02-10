e2d52e9c5042a0dd216ecf03f58010b2dbe93e7c
srinandangeek <60738636+srinandangeek@users.noreply.github.com>
2020-02-10 12:59:01 +0530

    Update HelloController.java
    reverted message

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot after 1st update!";
+        return "Hello from Spring Boot!";
     }   
 }
